798cfb11a57d35efd627e17fa4fb70715fcc0e8f
dingxiaobo <dingxiaobo@xiaomi.com>
2020-10-13 19:45:16 +0800

    Ensure mNoFocusedWindowTimeoutTime is null when new Focused Window come
    Now mNoFocusedWindowTimeoutTime doesn't reset when new Focused Window come, only new KeyEvent or FocusApp change will reset.
    if pendingEvent was Pruned due to new MotionEvent  and focusWindow come before ANR timeout,
    the ANR of no Focused Window still happen because mNoFocusedWindowTimeoutTime no reset.
    
    Change-Id: I0c86ade4c7d68f00426d4c431be4ffb3d79c01f2
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>

--- services/inputflinger/dispatcher/InputDispatcher.cpp
+++ services/inputflinger/dispatcher/InputDispatcher.cpp
@@ -3726,6 +3726,7 @@ void InputDispatcher::setInputWindowsLocked(
                       newFocusedWindowHandle->getName().c_str(), displayId);
             }
             mFocusedWindowHandlesByDisplay[displayId] = newFocusedWindowHandle;
+	    resetNoFocusedWindowTimeoutLocked();
             enqueueFocusEventLocked(*newFocusedWindowHandle, true /*hasFocus*/);
         }
 
